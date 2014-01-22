5021e2b5e17bda8c3cb452565464d3a1225b75b9
Jonas Drewsen <jdrewsen@gmail.com>
2014-01-22 21:59:07 +0100

    Improve project tool

--- tool.d
+++ tool.d
@@ -1,3 +1,4 @@
+import std.algorithm;
 import std.conv;
 import std.file;
 import std.path;
@@ -5,25 +6,106 @@ import std.process;
 import std.stdio;
 import std.string;
 import std.getopt;
+import std.array;
 
 void usage()
 {
-	write("""tool [-h] [--batch|-b] [--uploadTo|-u dest] <-outputVersion|-o ver> 
-		""");
+	auto usageTmpl = """tool [-h] <command> [command options...]
+Commands
+  setup  : Setup development env. e.g. create VS project files.
+  build  : Compile and link libraries and executables
+  test   : Run tests (building if necessary) optional filter can be provided
+  dist   : Create and/or upload installer e.g. tool dist 1.3
+""";
+ write(usageTmpl);
 }
 
 void main(string[] args)
 {
+	if (args.length < 2)
+	{
+		usage();
+		return;
+	}
+	else if (["-h", "/h", "/?", "--help", "/help"].count(args[1].toLower()))	
+	{
+		usage();
+		return;
+	}
+
+	switch (args[1])
+	{
+		case "setup":
+			setup(args);
+			return;
+		case "build":
+			build(args);
+			return;
+		case "test":
+			test(args);
+			return;
+		case "dist":
+			dist(args);
+			return;
+		case "help":
+			usage();
+			return;
+		default:
+			break;
+	}
+	write("Unknown command : " ~ args[1] ~ ". Use -h for help");
+}
+
+void setup(string[] args)
+{
+	auto cmd = "dub generate visuald";
+	writeln(cmd);
+	auto res = executeShell(cmd);
+	writeln(format("status %s:", res.status));
+	writeln(res.output);
+}
+
+void build(string[] args)
+{
+	auto cmd = "dub build 2>&1";
+	writeln(cmd);
+	auto res = pipeShell(cmd, Redirect.stdin);
+	wait(res.pid);
+//	writeln(format("status %s:", res.status));
+//	writeln(res.output);
+}
+
+void test(string[] args)
+{
+	
+	string filt = args.length > 2 ? args[2] : "";
+	auto cmd = "dub run --config=unittest";
+	writeln(cmd);
+	auto res = pipeShell(cmd, Redirect.stdin | Redirect.stderrToStdout | Redirect.stdout);
+	foreach (line; res.stdout.byLine)
+	{
+		if (filt.empty || line.startsWith("0x") || !line.find(filt).empty || !line.toLower().find("exception").empty)
+		{
+			writeln(line);
+		}
+	}
+	wait(res.pid);
+}
+
+void dist(string[] args)
+{
 	bool batchMode = false;
 	bool showUsage = false;
+	bool skipUpload = false;
 	string outputVersion = null;
 	auto uploadPath = "jcd@freeze.steamwinter.com:webdownloads/";
 
 	getopt(args,
 		"help|h", &showUsage,
 		"batch|b", &batchMode,
-		"outputVersion|o", &outputVersion,
-		"uploadTo|u", &uploadPath);
+		// "outputVersion|o", &outputVersion,
+		"uploadTo|u", &uploadPath,
+		"skipUpload|s", &skipUpload);
 
 	if (showUsage)
 	{
@@ -31,11 +113,13 @@ void main(string[] args)
 		return;
 	}
 
-	if (outputVersion is null)
+	if (args.length < 3)
 	{
-		writeln("Version argument must be specified e.g. 0.1");
+		writeln("Version argument must be specified e.g. 'tool dist 0.1'");
 		return;
 	}
+
+	outputVersion = args[2];
 
 	auto osStr = to!string(std.system.os);
 	auto outputPath = buildPath("dist", format("ded-%s_%s.zip", outputVersion, osStr));
@@ -48,8 +132,10 @@ void main(string[] args)
 	mkdirRecurse(packRoot);
 
 	collect(packRoot);
+	std.file.write(buildPath(packRoot, "version"), outputVersion);
 	archive(packRoot, outputPath);
-	upload(outputPath, uploadPath);
+	if (!skipUpload)
+		upload(outputPath, uploadPath);
 }
 
 void collect(string packRoot)
@@ -70,7 +156,7 @@ void collect(string packRoot)
 
 	foreach (f; files)
 	{
-		string dest =  buildPath(packRoot, f);
+		string dest = buildPath(packRoot, f);
 		std.stdio.writeln(f, " => ", dest);
 		if (exists(dest))
 			remove(dest);
@@ -109,15 +195,15 @@ void archive(string packRoot, string outputPath)
 		writeln("Removing existing ", outputPath);
 		remove(outputPath);
 	}
-	
+
 	auto cmd = format("%s a -r %s %s", zipcmd, outputPath, srcDir);
 	writeln(cmd);
-	auto res = executeShell(cmd);
+	auto res = pipeShell(cmd, Redirect.stdin);
+	int status = wait(res.pid);
 
-	if (res.status != 0)
+	if (status != 0)
 	{
-		writeln(format("status %s:", res.status));
-		writeln(res.output);
+		writeln(format("status %s:", status));
 	}
 	else
 	{

ab9c8945de1924c10b1dd71e15d82e28cd133860
Jonas Drewsen <jdrewsen@gmail.com>
2014-02-11 12:13:37 +0100

    Added expansion/contraction of regions based on point of insertion/deletion. Added new intersect method to region that returns the three parts of a intersection. Added length property to region.

--- src/math/region.d
+++ src/math/region.d
@@ -4,6 +4,7 @@ module math.region;
 import alg = std.algorithm;
 import std.container;
 import std.exception;
+import std.typecons;
 
 import core.buffer;
 
@@ -24,6 +25,11 @@ struct Region
 	@property bool empty() const
 	{ 
 		return a == b; 
+	}
+
+	@property pure size_t length() const nothrow
+	{
+		return b - a;
 	}
 
 	unittest 
@@ -63,6 +69,19 @@ struct Region
 		if (r.b <= a || r.a >= b || r.empty || empty) 
 			return Region(a, a, id); // an empty region with start at this.a
 		return Region(r.a > a ? r.a : a, r.b < b ? r.b : b, id);
+	}
+
+	/** Return the part before intersection as idx 0, the intersection itself as idx 1 and the part after as idx 2.
+		In case of no intersection the before part contains this region and at and after are empty regions.
+	*/
+	Tuple!(Region, "before", Region, "at", Region, "after") intersect3(Region r) const
+	{
+		Region r1 = intersect(r);
+		if (r1.empty)
+			return Tuple!(Region, "before", Region, "at", Region, "after")(this, zero, zero);
+		Region r0 = Region(a, r1.a);
+		Region r2 = Region(r1.b, b);
+		return Tuple!(Region, "before", Region, "at", Region, "after")(r0, r1, r2);
 	}
 	
 	unittest {
@@ -114,13 +133,148 @@ struct Region
 		Assert(!Region(0,1).contains(1));
 		Assert(!Region(0,1).contains(2));
 	}
+
+	/** Update all affected regions by shifting their endpoints or expanding because of new 
+	    entries are inserted on what the regions describe
+
+		Params:
+			pos = position to remove from
+			len = number of entries to remove forward from pos
+
+		Returns: True if this regions was modified
+	*/
+	bool entriesInserted(uint pos, uint len)
+	{
+		bool modified = false;
+		if (a >= pos)
+		{
+			// All regions strictly after the text inserted
+			// TODO: when array ref bug is fixed then do it proper
+			a += len;
+			b += len;
+			modified = true;
+		} 
+		else if (b >= pos)
+		{
+			// Text is inserted in the middle of this region
+			b += len;
+			modified = true;
+		}
+		return modified;
+	}
+	
+	/** Update all affected regions by shifting their endpoints or expanding because of  
+	    entries are removed on what the regions describe
+
+		Params:
+			pos = position to remove from
+			len = number of entries to remove forward from pos
+	
+		Returns: True if this regions was modified
+	*/
+	bool entriesRemoved(uint pos, uint len)
+	{
+		bool modified = false;
+		if (a >= (pos+len))
+		{
+			// All regions strictly after the text inserted
+			// TODO: when array ref bug is fixed then do it proper
+			a -= len;
+			b -= len;
+			modified = true;
+		} 
+		else if (b >= (pos+len))
+		{
+			// Text is deleted in the middle of this region with b outside
+			a = a > pos ? pos : a;
+			b -= len;
+			modified = true;
+		}
+		else if (b >= pos)
+		{
+			// Text is deleted in the middle of this region with b inside
+			a = a > pos ? pos : a;
+			b = pos;
+			modified = true;
+		}
+		return modified;
+	}
+
+	/+
+	/** Returns two regions in a struct called before, after where zero, one or both may be empty */
+	auto subtract(Region r)
+	{
+		auto i = intersect3(r);
+		struct Result
+		{
+			Region before;
+			Region after;
+		}
+		Result result;
+
+		if (i.at.empty)
+		{
+			// no intersection
+			result.before = this;
+		}
+		else if (i.before.empty)
+		{
+			if (i.after.empty)
+				result.before = Region(0,0); // All intersects
+			else
+				result.after = i.after; 
+		}
+
+		if (i.empty)
+		{
+			result.after = this;
+			return result;
+		}
+
+		// ? - ? --- ? -- ?
+
+		if (r.a < a )
+		{
+			// r.a - a --- ? -- ?
+			if (r.b < a)
+			{
+				// r.a - r.b --- a -- b
+				result.after = this;
+			}
+			else if (r.b < b)
+			{
+				// r.a - a --- r.b -- b
+				result.after = Region(r.b, b);
+			}
+			else
+			{
+				// r.a - a --- b -- r.b
+				// empty result
+			}
+		}
+		else
+		{
+			// a - r.a --- ? -- ?
+			if (r.a >= b)
+			{
+				result.before = this;
+				// no in
+			}
+			else
+			{
+				
+			}
+		}
+	}
++/
 }
 
 /** A set of regions
  * 
- * The regions is not overlapping and adding a region that
+ * The regions are not overlapping and adding a region that
  * overlaps existing regions in the set will merge them into one.
  * 
+ * TODO: Make merge an optional feature
  * TODO: Make operations lazy by returning a Range instead of a new RegionSet
  */
 class RegionSet
@@ -155,6 +309,72 @@ class RegionSet
 		assert(mergeIntersectingRegions);
 	}
 
+	// Update all affected regions by shifting their endpoints or expanding because of new 
+	// entries are inserted on what the regions describe
+	void entriesInserted(uint pos, uint len)
+	{
+		foreach (i; 0..regions.length)
+		{
+			auto cur = regions[i];
+			if (cur.entriesInserted(pos, len))
+				regions[i] = cur;
+/+
+			if (cur.a >= pos)
+			{
+				// All regions strictly after the text inserted
+				// TODO: when array ref bug is fixed then do it proper
+				cur.a += len;
+				cur.b += len;
+				regions[i] = cur;
+			} 
+			else if (cur.b >= pos)
+			{
+				// Text is inserted in the middle of this region
+				cur.b += len;
+				regions[i] = cur;
+			}
+			+/
+		}
+	}
+
+	// Update all affected regions by shifting their endpoints or contracting because of  
+	// entries are removed on what the regions describe
+	void entriesRemoved(uint pos, uint len)
+	{
+		foreach (i; 0..regions.length)
+		{
+			auto cur = regions[i];
+			if (cur.entriesRemoved(pos, len))
+				regions[i] = cur;
+		}
+	}
+
+	/*
+	// Update all affected regions by shifting their endpoints or contracting because of  
+	// entries are removed on what the regions describe
+	void entriesRemoved(uint pos, uint len)
+	{
+		foreach (i; 0..regions.length)
+		{
+			auto cur = regions[i];
+			if (cur.a >= pos)
+			{
+				// All regions strictly after the text inserted
+				// TODO: when array ref bug is fixed then do it proper
+				cur.a -= len;
+				cur.b -= len;
+				regions[i] = cur;
+			} 
+			else if (cur.b >= pos)
+			{
+				// Text is inserted in the middle of this region
+				int posToEndDiff = cur.b - pos;
+				cur.b -= posToEndDif < len ? posToEndDiff : len;
+				regions[i] = cur;
+			}
+		}		
+	}
+*/
 	void add(uint a, uint b, int id = 0)
 	{
 		add(Region(a, b, id));
@@ -182,6 +402,8 @@ class RegionSet
 	
 	void add(Region r)
 	{ 
+		if (r.empty) return;
+		
 		int mergeStartIdx = -1;
 		
 		// Unify this region with all the regions that it
@@ -260,8 +482,74 @@ class RegionSet
 		s1.add(Region(10,15));
 		Assert(s1.length, 1);
 		Assert(s1.front, Region(0,35));
+
+		auto s2 = new RegionSet;
+		s2.add(10, 20);
+		s2.add(3, 7);
+		Assert(s2.length, 2);
+		Assert(s2[0], Region(3,7));
+		Assert(s2[1], Region(10,20));
+	}
+/*
+	void toggle(uint a, uint b, int id = 0)
+	{
+		toggle(Region(a, b, id));
 	}
 
+	void toggle(Region r)
+	{
+		if (r.empty) return;
+
+		int mergeStartIdx = -1;
+
+		// Unify this region with all the regions that it
+		// intersects
+		foreach (i; 0..regions.length)
+		{
+			auto cur = regions[i];
+			auto intersection = cur.intersect(r);
+
+			if (!intersection.empty)
+			{									
+				// 
+				r = cur.cover(r);
+				if (mergeStartIdx == -1)
+					mergeStartIdx = i; // remember the first merged index into regions
+
+				// Continue to see if any of the next regions is to be merged
+			}
+			else if (mergeStartIdx != -1)
+			{
+				// The region has been merged with regions before current one and
+				// does not intersect with current one. This means we're done and just need
+				// to replace the merged region with the regions that it is overlapping.
+				regions.replace(regions[mergeStartIdx..i], r);
+				return;
+			}
+			else if (r.b < cur.a) 
+			{
+				// No intersection and nothing merged so far but the current region is after
+				// r ie. we need to insert r before current region.
+				regions.insertBefore(regions[i..regions.length], r);
+				return;
+			} 
+			// keep looking for a spot to insert r.
+
+		}
+
+		// We've reached the last element in region	
+		if (mergeStartIdx == -1)
+		{
+			// Nothing merged so just push back
+			regions.insertBack(r);
+		}		
+		else
+		{
+			// Something to merge until the very end
+			regions.replace(regions[mergeStartIdx..regions.length], r); // replace merged
+		}	
+	}
+*/
 	void addx(Region r)
 	{ 
 		int mergeStartIdx = -1;

b11770e18c3f67be98109aba6336ed0d2aced841
Jonas Drewsen <jdrewsen@gmail.com>
2014-02-25 21:45:22 +0100

    Improve test module with stats

--- src/test.d
+++ src/test.d
@@ -1,20 +1,55 @@
 module test;
 
 import std.stdio;
+import std.string;
+import std.conv;
+
+static 
+{
+	int g_Total = 0;
+	struct TestRecord
+	{
+		bool success;
+		string assertion;
+		string msg;
+		string file;
+		int line; 
+	}
+	alias TestRecord[] TestRecords;
+	TestRecords g_TestRecords;
+}
+
+void recordTestResult(bool success, string assertion, string msg, string file, int line)
+{
+	g_Total++;
+	g_TestRecords ~= TestRecord( success, assertion, msg, file, line );
+}
+
+void printStats(bool includeSuccessful = false)
+{
+	import std.algorithm;
+	foreach (rec; g_TestRecords)
+	{
+		if (includeSuccessful || !rec.success)
+			writeln("Test ", rec.file[4..$], ":", rec.line, " ", rec.msg, " ", rec.assertion, " ", rec.success ? "OK" : "FAILED");
+	}
+	writeln(format("%s of %s OK", g_TestRecords.count!"a.success", g_Total));
+}
 
 void Assert(lazy bool expMustBeTrue, string msg = "", string file = __FILE__, int line = __LINE__)
 {
 	bool res = expMustBeTrue();
-	writeln("Test ", file, "@", line, " ", msg, ": ", res ? "OK" : "FAILED");
+	recordTestResult(res, "", msg, file, line);
+	// writeln("Test ", file, "@", line, " ", msg, ": ", res ? "OK" : "FAILED");
 }
-
 
 void Assert(T,V)(lazy T thisExp, lazy V isEqualToThisExp, string msg = "", string file = __FILE__, int line = __LINE__)
 {
 	auto a = thisExp();
 	auto b = isEqualToThisExp();
 	bool res = a == b;
-	writeln("Test ", file, "@", line, " ", msg, ": ", a, " == ", b, " ", res ? "OK" : "FAILED");
+	recordTestResult(res, text(a, " == ", b), msg, file, line);
+	// writeln("Test ", file, "@", line, " ", msg, ": ", a, " == ", b, " ", res ? "OK" : "FAILED");
 } 
 
 immutable string ANSI_RED = "\x1b[31m";
@@ -27,6 +62,7 @@ void AssertIs(T,V)(lazy T thisExp, lazy V isEqualToThisExp, string msg = "", str
 	auto a = thisExp();
 	auto b = isEqualToThisExp();
 	bool res = a is b;
-//	writeln("Test ", file, "@", line, " ", msg, ": ", a, " is ", b, " ", res ? ANSI_GREEN ~ "OK" ~ ANSI_RESET : ANSI_RED ~ "FAILED" ~ ANSI_RESET);
-	writeln("Test ", file, "@", line, " ", msg, ": ", a, " is ", b, " ", res ? "OK" : "FAILED");
+	recordTestResult(res, text(a, " is ", b), msg, file, line);
+	//	writeln("Test ", file, "@", line, " ", msg, ": ", a, " is ", b, " ", res ? ANSI_GREEN ~ "OK" ~ ANSI_RESET : ANSI_RED ~ "FAILED" ~ ANSI_RESET);
+	// writeln("Test ", file, "@", line, " ", msg, ": ", a, " is ", b, " ", res ? "OK" : "FAILED");
 }

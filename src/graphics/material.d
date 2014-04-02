c416f38c7f22adba60493c91f53c0d925d91825f
Jonas Drewsen <jdrewsen@gmail.com>
2014-04-02 16:22:03 +0200

    Move shader and texture into properties on material

--- src/graphics/material.d
+++ src/graphics/material.d
@@ -19,16 +19,46 @@ class Material
 		return builtIn_;
 	}
 	
-	ShaderProgram shader;
-	Texture texture;
+	protected ShaderProgram _shader;
+	protected Texture _texture;
+
+	@property ShaderProgram shader()
+	{
+		return _shader;
+	}
+
+	@property void shader(ShaderProgram s)
+	{
+		_shader = s;
+	}
+
+	@property Texture texture()
+	{
+		return _texture;
+	}
+
+	@property void texture(Texture t)
+	{
+		_texture = t;
+	}
 	
+	@property bool hasTexture() const
+	{
+		return _texture !is null;
+	}
+
+	@property bool hasShader() const
+	{
+		return _shader !is null;
+	}
+
 	static Material create(const(char)[] imagePath)
 	{
 		Texture tex = Texture.create(imagePath);
 		
 		Material mat = new Material();
-		mat.texture = tex;
-		mat.shader = ShaderProgram.builtIn;
+		mat._texture = tex;
+		mat._shader = ShaderProgram.builtIn;
 		return mat;
 	}
 	

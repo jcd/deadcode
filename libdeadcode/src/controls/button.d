6416a6d59836f346cca0a84637b4e85ac85315ea
Jonas Drewsen <jdrewsen@gmail.com>
2015-01-17 13:20:49 +0100

    Made Button work correctly with styling. Added ToggleButton.

--- libdeadcode/src/controls/button.d
+++ libdeadcode/src/controls/button.d
@@ -5,46 +5,103 @@ import gui.label;
 import gui.style;
 import gui.widget;
 
+import core.signals;
+
 class Button : Label
 {
+	enum State
+	{
+		notLoaded,
+		loadedWithMouseOver,
+		loadedWithNoMouseOver
+	}
+	private State _state = State.notLoaded;
+
+	//mixin Signal!(Button) onActivated;
+
 	this(string text)
 	{
 		super(text);
 	}
 
+	static Button isover;
+
 	override EventUsed onEvent(Event event)
 	{
-		EventUsed used = void;
+		EventUsed used;
 		switch (event.type)
 		{
 			case EventType.MouseDown:
-				used = EventUsed.yes;
+				_state = State.loadedWithMouseOver;
+				used = EventUsed.no;
 				break;
 			case EventType.MouseUp:
-				used = EventUsed.yes;
+				if (_state == State.loadedWithMouseOver)
+					activate();	
+				_state = State.notLoaded;					
+				used = EventUsed.no;
 				break;
 			case EventType.MouseClick:
-				used = EventUsed.yes;
+				used = EventUsed.no;
 				break;
 			case EventType.MouseDoubleClick:
-				used = EventUsed.yes;
+				used = EventUsed.no;
 				break;
 			case EventType.MouseTripleClick:
-				used = EventUsed.yes;
+				used = EventUsed.no;
 				break;
 			case EventType.MouseMove:
-				used = EventUsed.yes;
+				used = EventUsed.no;
 				break;
 			case EventType.MouseOver:
-				used = EventUsed.yes;
+				if (_state == State.loadedWithNoMouseOver)
+					_state = State.loadedWithMouseOver;
+				isover = this;
+				used = EventUsed.no;
 				break;
 			case EventType.MouseOut:
-				used = EventUsed.yes;
+				if (_state == State.loadedWithMouseOver)
+					_state = State.loadedWithNoMouseOver;
+				used = EventUsed.no;
 				break;
 			default:
-				used = super.onEvent(event);
+				//used = super.onEvent(event);
 				break;
 		}
+		
+		//if (!used)
+			used = super.onEvent(event);
 		return used;
 	}
+
+	// TODO: This is a hack because std.signals does not support derived signals
+	protected void activate()
+	{
+	}
 }
+
+class ToggleButton : Button
+{
+	bool isOn = false;
+	mixin Signal!(ToggleButton) onToggled;
+	
+	enum _classes = [["on"],["off"]];
+
+	override protected @property const(string[]) classes() const pure nothrow @safe
+	{
+		return _classes[isOn ? 0 : 1];
+	}
+
+	this(string text)
+	{
+		super(text);
+		// onActivated.connect(&activate);
+	}
+
+	override protected void activate()
+	{
+		isOn = !isOn;
+		onToggled.emit(this);
+	}
+}
+

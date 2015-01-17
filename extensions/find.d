2e9af0302e452f35dea22895c252dba51a30adc5
Jonas Drewsen <jdrewsen@gmail.com>
2015-01-17 14:26:18 +0100

    Find in files extension.

---
+++ extensions/find.d
@@ -0,0 +1,51 @@
+module extensions.find;
+
+import extensions.attr;
+mixin registerCommands;
+
+@Shortcut("<ctrl> + <shift> + f")
+void findInFiles(string needle)
+{
+	import std.file;
+	import std.algorithm;
+	import std.string;
+	import std.utf;
+
+	struct FoundItem
+	{
+		string filePath;
+		ptrdiff_t index;
+	}
+	FoundItem[] foundItems;
+	import std.stdio;
+
+	// Iterate over all *.d files in current directory and all its subdirectories
+	auto dFiles = filter!`endsWith(a.name,".d")`(dirEntries(".",SpanMode.depth));
+	foreach(d; dFiles)
+	{
+		string haystack;
+		try
+		{
+			haystack = readText(d.name);
+		} 
+		catch (UTFException)
+		{
+			// TODO: Remember these files and skip on next find
+			writeln("Cannot find in file ", d.name);
+			continue;
+		}
+		ptrdiff_t idx = haystack.indexOf(needle);
+		
+		while (idx >= 0)
+		{
+			foundItems ~= FoundItem(d.name, idx);
+			if (idx != haystack.length-1)
+				idx = haystack.indexOf(needle, idx+1);
+			else
+				idx = -1;
+		}
+	}
+
+	foreach (i; foundItems)
+	writeln(i.filePath, " ", i.index);
+}

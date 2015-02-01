ede486b77750d1025f35812cd5882ea17a48154b
Jonas Drewsen <jdrewsen@gmail.com>
2015-02-01 14:36:36 +0100

    Command to create a new extension widget class.

--- extensions/createfile.d
+++ extensions/createfile.d
@@ -1,6 +1,6 @@
 module extensions.createfile;
 
-import extensions.attr;
+import extension;
 mixin registerCommands;
 
 @MenuItem("New/Buffer") 
@@ -23,13 +23,50 @@ void newExtension(GUIApplication app)
 	// b.name = "New extension";
 	b.insert(q"{module extensions.myext;
 
-import extensions.attr;
+import extension;
 mixin registerCommands;
 
 void myextHello(BufferView v)
 {
 	v.insert("Hello");
 }
+}");
+	
+	ICodeIntel i = manager().lookup("D");
+	ICodeModel m = i.createModel(b);
+	b.codeModel = m;
+
+	app.showBuffer(b);
+}
+
+void newWidget(GUIApplication app)
+{
+	import core.language;
+	auto b = app.createBuffer();
+	
+	import std.path;
+	
+	//auto base = buildNormalizedPath(app.resourceURI("./", ResourceBaseLocation.currentDir).uriString, "extensions/myext.d");
+	//b.name = base;
+	// b.name = "New extension";
+	b.insert(q"{module extensions.mywidget;
+
+import extension;
+mixin registerCommands;
+
+class MyWidget : BasicWidget
+{
+	Label label;
+	Button button;
+
+	override void init()
+	{
+		label = new Label("Hello");
+		button = new Button("Click");
+		button.onActivated ~- (b) { label.text = "World"; };
+	}
+}
+
 }");
 	
 	ICodeIntel i = manager().lookup("D");

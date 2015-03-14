e29c79613216bedd7810fc36733bf2f72bf7a99a
Jonas Drewsen <jdrewsen@gmail.com>
2015-03-14 22:28:49 +0100

    New proc extension to filter text/selection through external shell script

---
+++ extensions/proc.d
@@ -0,0 +1,61 @@
+module extensions.proc;
+
+import extensions;
+mixin registerCommands;
+
+void procFilter(GUIApplication app, BufferView bv, string batchFile)
+{
+    import std.array;
+    import std.process;
+	import std.file;
+
+    if (!exists(batchFile))
+    {
+        app.addMessage("No such batch file " ~ batchFile);
+        return;
+    }
+
+    auto res = pipeProcess(batchFile);
+
+    res.stdin.write(bv.getText(0, bv.length));
+    res.stdin.flush();
+    res.stdin.close();
+
+    auto apnd = appender!(ubyte[])();
+
+    foreach (ubyte[] chunk; res.stdout.byChunk(4096))
+	    apnd.put(chunk);
+
+    if (wait(res.pid) != 0)
+    {
+	    app.addMessage("Error running batch file " ~ batchFile);
+    }
+    else
+    {
+        bv.clear(cast(string)apnd.data);
+    }
+}
+
+void procRunCommands(GUIApplication app, BufferView bv, string cmdFile)
+{
+    import std.array;
+    import std.exception;
+    import std.stdio;
+	import std.file;
+    import std.string;
+
+    if (!exists(cmdFile))
+    {
+        app.addMessage("No such batch file " ~ cmdFile);
+        return;
+    }
+
+	foreach (l; File(cmdFile).byLine())
+    {
+		app.addMessage("Running " ~ l);
+		auto cmdName = munch(l, "a-zA-Z_.");
+        munch(l, " \t");
+		collectExceptionMsg(app.commandManager.parseArgumentsAndExecute(cmdName.idup, l.idup));
+    }
+}
+

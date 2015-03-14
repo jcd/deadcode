62f07a6e14e3b16450b3509243ebd1552d8223ce
Jonas Drewsen <jdrewsen@gmail.com>
2015-03-14 22:16:34 +0100

    Fix gridlayout to handle min/max/preferred sizing.

--- libdeadcode/src/gui/layout/gridlayout.d
+++ libdeadcode/src/gui/layout/gridlayout.d
@@ -17,6 +17,8 @@ class GridLayout : ILayout
 {
 	float[] fixedRowSizes;    // if < 0 then row is not fixed
 	float[] fixedColumnSizes; // if < 0 then column is not fixed
+    float cellHorizontalSpacing = 0f;
+    float cellVerticalSpacing = 0f;
 
 	int _count = 1;
 
@@ -47,8 +49,17 @@ class GridLayout : ILayout
 		Rectf rect = Rectf(wrect.x + padding.left, wrect.y + padding.top,
 						   wrect.w - padding.left - padding.right, wrect.h - padding.top - padding.bottom);
 
-		auto rows = _direction == Direction.row ? _count : children.length / _count;
-		auto cols = _direction == Direction.row ? children.length / _count : _count;
+
+		auto rows = _direction == Direction.row ? _count : cast(int)(ceil(float(children.length) / float(_count)));
+		auto cols = _direction == Direction.row ? cast(int)(ceil(float(children.length) / float(_count))) : _count;
+
+        auto hSpace = max(0, cols - 2);
+        auto vSpace = max(0, rows - 2);
+        auto hSpacing = hSpace * cellHorizontalSpacing;
+        auto vSpacing = vSpace * cellVerticalSpacing;
+
+        rect.w -= hSpacing;
+        rect.h -= vSpacing;
 
 		if (rows > fixedRowSizes.length)
 		{
@@ -64,12 +75,19 @@ class GridLayout : ILayout
 			fixedColumnSizes[fixedColumnSizes.length - d..$] = -1f;
 		}
 
-		float[] colWidths;
+		struct MinMaxPreferred
+        {
+            float min;
+            float max;
+            float preferred;
+        }
+
+        MinMaxPreferred[] colWidths;
 		colWidths.length = cols;
-		colWidths[] = 0f;
-		float[] rowHeights;
+		colWidths[] = MinMaxPreferred(-float.infinity, float.infinity, 0f);
+		MinMaxPreferred[] rowHeights;
 		rowHeights.length = rows;
-		rowHeights[] = 0f;
+		rowHeights[] = MinMaxPreferred(-float.infinity, float.infinity, 0f);
 
 		foreach (i, w; children)
 		{
@@ -78,45 +96,230 @@ class GridLayout : ILayout
 			auto column = i % cols;
 			auto row = i / cols;
 
-			rowHeights[row] = fixedRowSizes[row] < 0f ? max(rowHeights[row], w.h) : fixedRowSizes[row];
-			colWidths[column] = fixedColumnSizes[column] < 0f ? max(colWidths[column], w.w) : fixedColumnSizes[column];
-		}
-
-		//float accumFixedWidth = reduce( (a,b) => b > 0 ? a+b : a )(0f, fixedColumnSizes);
-		//float accumFixedHeight = reduce( (a,b) => b > 0 ? a+b : a )(0f, fixedRowSizes);
-		//float accumFlexWidth = accumWidth - accumFixedWidth;
-		//float accumFlexHeight = accumHeight - accumFixedHeight;
-		float accumWidth = sum(colWidths);
-		float accumHeight = sum(rowHeights);
-		float fixedWidth = reduce!((acc,elm) => elm < 0 ? acc : acc + elm)(0f, fixedColumnSizes);
-		float fixedHeight = reduce!((acc,elm) => elm < 0 ? acc : acc + elm)(0f, fixedRowSizes);
-
-		//float widthLeft = rect.w - accumWidth;
-		//float heightLeft = rect.h - accumHeight;
-
-		// We scale only the flex part of the col/row in case the accumulated size it too large/small
-		// TODO: handle when width/heightLeft is negatively larger that accumWidth/Height
-		float accumFlexWidth  = accumWidth - fixedWidth;
-        float accumFlexHeight = accumHeight - fixedHeight;
-        float scaleFlexWidth  = accumFlexWidth.isNormal ? (rect.w - fixedWidth ) / accumFlexWidth : 0f;
-		float scaleFlexHeight = accumFlexHeight.isNormal ? (rect.h - fixedHeight) / accumFlexHeight : 0f;
-
-		foreach (i; 0..rows)
-		{
-			if (fixedRowSizes[i] < 0)
-				rowHeights[i] = rowHeights[i] * scaleFlexHeight;
-            import std.math;
-            if (!isFinite(rowHeights[i]))
+            float fixedRowSize = fixedRowSizes[row];
+            const bool hasFixedRowSize = fixedRowSize >= 0f;
+            if (hasFixedRowSize)
             {
-                auto dd = rowHeights[i];
+                MinMaxPreferred* r = &(rowHeights[row]);
+                r.min = float.nan; // signal fixed height
+                r.preferred = fixedRowSize;
+            }
+            else
+            {
+                MinMaxPreferred* r = &(rowHeights[row]);
+                if (w.minHeight > r.min)
+                    r.min = w.minHeight;
+                if (w.maxHeight < r.max)
+                    r.max = w.maxHeight;
+                r.preferred = min(max(r.preferred, w.h, r.min), r.max);
+            }
+
+            float fixedColumnSize = fixedColumnSizes[column];
+            const bool hasFixedColumnSize = fixedColumnSize >= 0f;
+            if (hasFixedColumnSize)
+            {
+                MinMaxPreferred* r = &(colWidths[column]);
+                r.min = float.nan; // signal fixed width
+                r.preferred = fixedColumnSize;
+            }
+            else
+            {
+                MinMaxPreferred* r = &(colWidths[column]);
+                if (w.minWidth > r.min)
+                    r.min = w.minWidth;
+                if (w.maxWidth < r.max)
+                    r.max = w.maxWidth;
+                r.preferred = min(max(r.preferred, w.w, r.min), r.max);
             }
 		}
 
-		foreach (i; 0..cols)
-		{
-			if (fixedColumnSizes[i] < 0)
-				colWidths[i] = colWidths[i] * scaleFlexWidth;
-		}
+        MinMaxPreferred flexWidth = MinMaxPreferred(0f, 0f, 0f);
+        float fixedWidth = 0f;
+        foreach (row, ref r; colWidths)
+        {
+            float fixedColumnSize = fixedColumnSizes[row];
+            const bool hasFixedColumnSize = fixedColumnSize >= 0f;
+            if (hasFixedColumnSize)
+            {
+                fixedWidth += fixedColumnSize;
+            }
+            else
+            {
+                flexWidth.min += r.preferred - r.min;
+                flexWidth.max +=  r.max - r.preferred;
+                flexWidth.preferred += r.preferred;
+            }
+        }
+        float deltaToPreferredWidth = rect.w - fixedWidth - flexWidth.preferred;
+
+        MinMaxPreferred flexHeight = MinMaxPreferred(0f, 0f, 0f);
+        float fixedHeight = 0f;
+        foreach (col, ref r; rowHeights)
+        {
+            float fixedRowSize = fixedRowSizes[col];
+            const bool hasFixedRowSize = fixedRowSize >= 0f;
+            if (hasFixedRowSize)
+            {
+                fixedHeight += fixedRowSize;
+            }
+            else
+            {
+                flexHeight.min += r.preferred - r.min;
+                flexHeight.max +=  r.max - r.preferred;
+                flexHeight.preferred += r.preferred;
+            }
+        }
+        float deltaToPreferredHeight = rect.h - fixedHeight - flexHeight.preferred;
+
+        //float scaleWidthFactor = 1f;
+        //if (deltaToPreferredWidth < 0f)
+        //{
+        //    // Need to scale down
+        //    if (-deltaToPreferredWidth > flexWidth.min)
+        //    {
+        //        // Delta is larger that how much we can flex down and we are going to break a constraint here!
+        //
+        //    }
+        //    scaleWidthFactor = (deltaToPreferredWidth + flexWidth.preferred) / flexWidth.preferred;
+        //}
+        //else if (deltaToPreferredWidth > 0f)
+        //{
+        //    if (deltaToPreferredWidth > flexWidth.max)
+        //    {
+        //        // Delta is larger that how much we can flex up and we are going to break a constraint here!
+        //    }
+        //    scaleWidthFactor = (deltaToPreferredWidth + flexWidth.preferred) / flexWidth.preferred;
+        //}
+
+
+        //
+        ////float accumFixedWidth = reduce( (a,b) => b > 0 ? a+b : a )(0f, fixedColumnSizes);
+        ////float accumFixedHeight = reduce( (a,b) => b > 0 ? a+b : a )(0f, fixedRowSizes);
+        ////float accumFlexWidth = accumWidth - accumFixedWidth;
+        ////float accumFlexHeight = accumHeight - accumFixedHeight;
+        //float accumWidth = sum(colWidths);
+        //float accumHeight = sum(rowHeights);
+        //
+        //float fixedWidth = reduce!((acc,elm) => elm < 0 ? acc : acc + elm)(0f, fixedColumnSizes);
+        //float fixedHeight = reduce!((acc,elm) => elm < 0 ? acc : acc + elm)(0f, fixedRowSizes);
+        //
+        ////float widthLeft = rect.w - accumWidth;
+        ////float heightLeft = rect.h - accumHeight;
+        //
+        //// We scale only the flex part of the col/row in case the accumulated size it too large/small
+        //// TODO: handle when width/heightLeft is negatively larger that accumWidth/Height
+        //float accumFlexWidth  = accumWidth - fixedWidth;
+        //float accumFlexHeight = accumHeight - fixedHeight;
+        //float scaleFlexWidth  = accumFlexWidth.isNormal ? (rect.w - fixedWidth ) / accumFlexWidth : 0f;
+        //float scaleFlexHeight = accumFlexHeight.isNormal ? (rect.h - fixedHeight) / accumFlexHeight : 0f;
+
+        //foreach (i; 0..rows)
+        //{
+        //    if (fixedRowSizes[i] < 0)
+        //        rowHeights[i] = rowHeights[i].preferred * scaleHeightFactor;
+        //    else
+        //        rowHeights[i] = fixedRowSizes[i];
+        //
+        //}
+
+        float lastDeltaToPreferredWidth = 0;
+        bool changed = true;
+        while (!approxEqual(deltaToPreferredWidth, lastDeltaToPreferredWidth, 1e-2, 0.1) && !approxEqual(deltaToPreferredWidth, 0f, 1e-2, 0.1) && changed)
+        {
+            lastDeltaToPreferredWidth = deltaToPreferredWidth;
+            changed = false;
+            float scaleWidthFactor = (deltaToPreferredWidth + flexWidth.preferred) / flexWidth.preferred;
+		    foreach (i; 0..cols)
+		    {
+			    MinMaxPreferred* c = &(colWidths[i]);
+                const isFixedWidthColumn = fixedColumnSizes[i] >= 0;
+                if (isFixedWidthColumn)
+                {
+                    c.preferred = fixedColumnSizes[i];
+                }
+                else if (!c.min.isNaN())
+                {
+                    float newWidth = c.preferred * scaleWidthFactor;
+                    float deltaWidth = newWidth - c.preferred;
+
+                    if (!scaleWidthFactor.isFinite)
+                    {
+                        // flexHeight.preferred was 0f. Just split deltaHeights evenly among the rows;
+                        newWidth = deltaToPreferredWidth / cols;
+                        deltaWidth = newWidth;
+                        c.min = float.nan; // signal stop of calc for this column
+                    }
+
+                    if (newWidth > c.max)
+                    {
+                        newWidth = c.max;
+                        deltaWidth = newWidth - c.preferred;
+                        c.min = float.nan; // signal stop of calc for this column
+                    }
+                    else if (newWidth < c.min)
+                    {
+                        newWidth = c.min;
+                        deltaWidth = newWidth - c.preferred;
+                        c.min = float.nan; // signal stop of calc for this column
+                    }
+
+                    flexWidth.preferred += deltaWidth;
+                    c.preferred = newWidth;
+                    deltaToPreferredWidth -= deltaWidth;
+                    changed = true;
+                }
+		    }
+        }
+
+
+        float lastDeltaToPreferredHeight = 0;
+        changed = true;
+        while (!approxEqual(deltaToPreferredHeight, lastDeltaToPreferredHeight, 1e-2, 0.1) && !approxEqual(deltaToPreferredHeight, 0f, 1e-2, 0.1) && changed)
+        {
+            lastDeltaToPreferredHeight = deltaToPreferredHeight;
+            changed = false;
+            float scaleHeightFactor = (deltaToPreferredHeight + flexHeight.preferred) / flexHeight.preferred;
+		    foreach (i; 0..rows)
+		    {
+			    MinMaxPreferred* c = &(rowHeights[i]);
+                const isFixedHeightRow= fixedRowSizes[i] >= 0;
+                if (isFixedHeightRow)
+                {
+                    c.preferred = fixedRowSizes[i];
+                }
+                else if (!c.min.isNaN())
+                {
+                    float newHeight = c.preferred * scaleHeightFactor;
+                    float deltaHeight = newHeight - c.preferred;
+
+                    if (!scaleHeightFactor.isFinite)
+                    {
+                        // flexHeight.preferred was 0f. Just split deltaHeights evenly among the rows;
+                        newHeight = deltaToPreferredHeight / rows;
+                        deltaHeight = newHeight;
+                        c.min = float.nan; // signal stop of calc for this row
+                    }
+
+                    if (newHeight > c.max)
+                    {
+                        newHeight = c.max;
+                        deltaHeight = newHeight - c.preferred;
+                        c.min = float.nan; // signal stop of calc for this row
+                    }
+                    else if (newHeight < c.min)
+                    {
+                        newHeight = c.min;
+                        deltaHeight = newHeight - c.preferred;
+                        c.min = float.nan; // signal stop of calc for this row
+                    }
+
+                    flexHeight.preferred += deltaHeight;
+                    c.preferred = newHeight;
+                    deltaToPreferredHeight -= deltaHeight;
+                    changed = true;
+                }
+		    }
+        }
 
 
 		// Scale the flex row and heights
@@ -135,12 +338,12 @@ class GridLayout : ILayout
 			if (column == 0)
 			{
 				offset.x = rect.pos.x;
-				offset.y = offset.y + (row == 0 ? 0f : rowHeights[row-1]);
+				offset.y = offset.y + (row == 0 ? 0f : rowHeights[row-1].preferred + cellVerticalSpacing);
 			}
 
 			w.pos = offset;
-			w.w = colWidths[column];
-			w.h = rowHeights[row];
+			w.w = colWidths[column].preferred;
+			w.h = rowHeights[row].preferred;
 
 			//// TODO: scale the flex cell according the max flex cell size
 			//if (fixedRowSize >= 0)
@@ -156,7 +359,7 @@ class GridLayout : ILayout
 			//else if (scaleFlexWidth != 1f)
 			//    w.w = w.w * scaleFlexWidth;
 
-			offset.x = offset.x + w.w;
+			offset.x = offset.x + w.w + cellHorizontalSpacing;
 		}
 
 		//auto rowChildren = widget.children;;

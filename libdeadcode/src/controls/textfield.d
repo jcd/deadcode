65332c9fa02b3f897c6dd79b8172df6d3e87b8a6
Jonas Drewsen <jdrewsen@gmail.com>
2015-03-14 22:39:04 +0100

    Make textfield constructor consistent. Get rid of custom preferredSize calculation.

--- libdeadcode/src/controls/textfield.d
+++ libdeadcode/src/controls/textfield.d
@@ -8,19 +8,19 @@ import math;
 
 class TextField : TextEditor
 {
-	override const(Vec2f) preferredSize()
-	{
-		auto ps = renderer.layoutSize;
-		if (ps.x != 0 && ps.y != 0)
-			return max(ps, size);
-		auto fontHeight = style.font.fontHeight;
+    //override const(Vec2f) preferredSize()
+    //{
+    //    auto ps = renderer.layoutSize;
+    //    if (ps.x != 0 && ps.y != 0)
+    //        return max(ps, size);
+    //    auto fontHeight = style.font.fontHeight;
+    //
+    //    return max(Vec2f(100, fontHeight), size);
+    //}
 
-		return max(Vec2f(100, fontHeight), size);
-	}
-
-	this(Widget parent, BufferView buf)
+	this(BufferView buf)
 	{
-		super(parent, buf);
+		super(buf);
 		bufferView.visibleLineCount = 1;
 	}
 

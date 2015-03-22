c8fab7ce9c9ca2f3531d48fc542e50a5464fdf41
Jonas Drewsen <jdrewsen@gmail.com>
2015-03-22 20:58:52 +0100

    New module loader class that can load a dll from embedded array or file

---
+++ libdeadcode/src/core/moduleloader.d
@@ -0,0 +1,86 @@
+module core.moduleloader;
+
+struct ModuleLoader(alias Mod, string modFilename)
+{
+    version (release)
+    {
+        enum dllBytes = cast(ubyte[]) import(modFilename);
+
+        bool load()
+        {
+            import std.stdio;
+            try
+            {
+                import std.path;
+                import std.file;
+
+                string depacked = buildPath(tempDir(), modFilename);
+                writeln("Unpacking to ", depacked);
+                std.file.write(depacked, dllBytes);                // Writing the dynlib to a temporary file.
+                Mod.load(depacked);                                // Use the depacked dynlib and load its symbols.
+            }
+            catch(Exception e)
+            {
+                writeln("Error loading ", Mod.stringof, " lib ", e);
+                return false;
+            }
+            return true;
+        }
+    }
+    else
+    {
+        bool load()
+        {
+            import std.stdio;
+            try
+            {
+                Mod.load();
+            }
+            catch(Exception e)
+            {
+                writeln("Error loading ", Mod.stringof, " lib ", e);
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
+struct ModuleLoaderRaw(string modFilename)
+{
+    version (release)
+    {
+        enum dllBytes = cast(ubyte[]) import(modFilename);
+
+        bool load()
+        {
+            import std.stdio;
+            try
+            {
+                import derelict.util.sharedlib;
+                import std.file;
+                import std.path;
+
+                string depacked = buildPath(tempDir(), modFilename);
+                writeln("Unpacking to ", depacked);
+                std.file.write(depacked, dllBytes);                // Writing the dynlib to a temporary file.
+                SharedLib sl;
+                sl.load([depacked]);
+                // Mod.load(depacked);                                // Use the depacked dynlib and load its symbols.
+            }
+            catch(Exception e)
+            {
+                writeln("Error loading ", modFilename, " lib ", e);
+                return false;
+            }
+            return true;
+        }
+    }
+    else
+    {
+        bool load()
+        {
+            return true;
+        }
+    }
+}

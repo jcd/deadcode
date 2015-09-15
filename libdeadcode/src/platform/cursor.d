3fc7a956829cf080e4e91dd29837e9bd999e7f79
Jonas Drewsen <jdrewsen@gmail.com>
2015-09-15 23:35:27 +0200

    use GetCursorPos() instead of GetCursorInfo()

--- libdeadcode/src/platform/cursor.d
+++ libdeadcode/src/platform/cursor.d
@@ -17,18 +17,33 @@ version (Windows)
     extern (Windows) nothrow
     {
         export BOOL GetCursorInfo(LPCURSORINFO lpPoint);
+        export BOOL GetCursorPos(
+                                 LPPOINT lpPoint
+                                 );
     }
 
     Vec2f getScreenPosition()
     {
+/*
         CURSORINFO desktopPos;
         desktopPos.cbSize = CURSORINFO.sizeof;
         if (! GetCursorInfo(&desktopPos))
         {
             std.stdio.writeln("errocode ", GetLastError());
         }
-
         Vec2f winPos = Vec2f(desktopPos.ptScreenPos.x, desktopPos.ptScreenPos.y);
+*/
+
+        POINT p;
+        GetCursorPos(&p);
+        Vec2f winPos = Vec2f(p.x, p.y);
+
+        /** When SDL2 2.0.4 is released the GetGlobalMouseState will be supported
+        import derelict.sdl2.sdl;
+        int x,y;
+        SDL_GetGlobalMouseState(&x, &y);
+        Vec2f winPos = Vec2f(x, y);
+*/
         return winPos;
     }
 }

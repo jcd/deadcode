b53a11ba2c909320d7626db5734b914e7d1192b3
Jonas Drewsen <jdrewsen@gmail.com>
2015-09-15 23:25:20 +0200

    Editor help command

--- extensions/editor.d
+++ extensions/editor.d
@@ -3,6 +3,31 @@ module extensions.editor;
 import extensions;
 mixin registerCommands;
 
+import std.algorithm;
+import std.format;
+import std.string;
+
+string helpText = q"{ Help
+------
+
+<ctrl> + p             : show command console
+<ctrl> + ,             : open file in dub project
+<ctrl> + x, <ctrl> + f : open file
+}";
+
+@MenuItem("Help")
+@Shortcut("<ctrl> + x <ctrl> + h")
+void help(GUIApplication app)
+{
+    enum helpBufferName = "*Help*";
+    app.addMessage("Showing help");
+
+    if (!app.hasBuffer(helpBufferName))
+        app.createBuffer(helpBufferName).insert(helpText);
+
+	app.showBuffer(helpBufferName);
+}
+
 @MenuItem("Quit")
 @Shortcut("<ctrl> + x <ctrl> + c")
 void editorQuit(GUIApplication app)

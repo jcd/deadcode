7283c89868c1f0c9880738e766cef02bf277e6c0
Jonas Drewsen <jdrewsen@gmail.com>
2015-09-16 01:49:17 -0700

    getExistingWindowRect implementation for linux

--- libdeadcode/src/platform/display.d
+++ libdeadcode/src/platform/display.d
@@ -12,25 +12,82 @@ version (Windows)
         nothrow export HWND FindWindowA(LPCTSTR className, LPCTSTR windowName);
     }
 
-    Rectf getExistingWindowRect()
+    bool getExistingWindowRect(Rectf* result)
     {
         auto hwnd = FindWindowA("SDL_app", "Deadcode");
-        Rectf result;
         RECT r;
         if (hwnd !is null)
         {
             GetWindowRect(hwnd, &r);
-            result = Rectf(r.left, r.top, r.right - r.left, r.bottom - r.top);
+            *result = Rectf(r.left, r.top, r.right - r.left, r.bottom - r.top);
+            return true;
         }
-        return result;
+        return false;
     }
 }
 
 version (linux)
 {
-    pragma(msg, "Warning: Missing getExistingWindowRect");
-    Rectf getExistingWindowRect()
+    import derelict.sdl2.sdl;  
+    bool getExistingWindowRect(Rectf* result)
     {
-        return Rectf(0,0, 500, 500);
+		import std.algorithm;
+        import x11.Xatom;
+        import x11.Xlib;
+        import x11.X;
+        
+        Display *display = XOpenDisplay(null);
+        scope (exit) XCloseDisplay(display);
+        assert(display);
+        Atom prop = XInternAtom(display,"WM_NAME",True);
+   
+        bool _get(Window w)
+        { 
+            // Get the PID for the current Window.
+        	ulong           type;
+        	int            format;
+        	ulong  nItems;
+        	ulong  bytesAfter;
+        	ubyte *propValue = null;
+        	if(XErrorCode.Success == XGetWindowProperty(display, w, prop, 0L, 1024L, False, XA_STRING,
+        	                                 &type, &format, &nItems, &bytesAfter, &propValue))
+        	{
+        		if(propValue !is null) 
+        		{
+        		    static import core.stdc.string;
+        		    auto l = core.stdc.string.strlen(cast(char*)propValue);
+        		    string name = (cast(char*)propValue)[0..l].idup;
+        		    import std.stdio;
+        		    writeln("Name : ", name);
+        			XFree(propValue);
+                    if (name.endsWith("deadcode"))
+                    {
+            		    XWindowAttributes xwa;
+                        XGetWindowAttributes(display, w, &xwa);
+                        result.x = xwa.x;
+                        result.y = xwa.y;
+                        result.w = xwa.width;
+                        result.h = xwa.height;
+	                    writeln("Found deadcode at", *result); 
+                        return true;
+                    }
+        		}
+        	}
+        	
+            // Recurse into child windows.
+        	Window    wRoot;
+        	Window    wParent;
+        	Window   *wChild;
+        	uint  nChildren;
+        	if(0 != XQueryTree(display, w, &wRoot, &wParent, &wChild, &nChildren))
+        	{
+        		for(uint i = 0; i < nChildren; i++)
+        			if (_get(wChild[i]))
+        			    return true;
+        	}
+        
+            return false;
+        }
+        return _get(XDefaultRootWindow(display));
     }
 }

e4a7c70a7f286439fdecf6a76c7015eb8ab608f9
Jonas Drewsen <jdrewsen@gmail.com>
2015-09-16 01:50:11 -0700

    getScreenPosition implementation for linux

--- libdeadcode/src/platform/cursor.d
+++ libdeadcode/src/platform/cursor.d
@@ -19,25 +19,56 @@ version (Windows)
         export BOOL GetCursorInfo(LPCURSORINFO lpPoint);
     }
 
-    Vec2f getScreenPosition()
+    bool getScreenPosition(Vec2f* result)
     {
         CURSORINFO desktopPos;
         desktopPos.cbSize = CURSORINFO.sizeof;
         if (! GetCursorInfo(&desktopPos))
         {
             std.stdio.writeln("errocode ", GetLastError());
+            return false;
         }
 
-        Vec2f winPos = Vec2f(desktopPos.ptScreenPos.x, desktopPos.ptScreenPos.y);
-        return winPos;
+        *result = Vec2f(desktopPos.ptScreenPos.x, desktopPos.ptScreenPos.y);
+        return true;
     }
 }
 
 version (linux)
 {
-    Vec2f getScreenPosition()
+    import derelict.sdl2.sdl;   
+    bool getScreenPosition(Vec2f* result)
     {
-        pragma(msg, "Warning: getScreenPosition not implemented for linux");
-        return Vec2f();
+        import x11.Xlib;
+        import x11.X;
+        
+        Window root;
+        Window child;
+        int root_x, root_y;
+        int win_x, win_y;
+        uint mask_return; 
+
+        Display *display = XOpenDisplay(null);
+        scope (exit) XCloseDisplay(display);
+
+//        Display *display = cast(Display*)info.info.x11.display;
+        assert(display);
+        // XSetErrorHandler(_XlibErrorHandler);
+        int number_of_screens = XScreenCount(display);
+        // fprintf(stderr, "There are %d screens available in this X session\n", number_of_screens);
+        for (int i = 0; i < number_of_screens; i++) {
+            Window win = XRootWindow(display, i);
+            Bool res = XQueryPointer(display, win, &root,
+                    &child, &root_x, &root_y, &win_x, &win_y,
+                    &mask_return);
+            if (res == True) {
+                XWindowAttributes root_attrs;
+                XGetWindowAttributes(display, root, &root_attrs);
+                result.x = root_attrs.x + root_x;
+                result.y = root_attrs.y + root_y;
+                return true;
+            }
+        }
+        return false;
     }
 }

40be10b764f7c393ad72ad9153390ad0507bf505
Jonas Drewsen <jdrewsen@gmail.com>
2015-09-29 10:02:08 +0200

    Do not register explorer context menu item on startup

--- libdeadcode/src/guiapplication.d
+++ libdeadcode/src/guiapplication.d
@@ -528,7 +528,7 @@ class GUIApplication : Application
 	{
 		import std.uuid;
 		import platform.config;
-        addFileBrowserContextMenuItem("Open with DeadCode", r"C:\Projects\D\ded\ded-debug_d.exe");
+        // addFileBrowserContextMenuItem("Open with DeadCode", r"C:\Projects\D\ded\ded-debug_d.exe");
         analyticsKey = getOrSetConfigField("analyticsKey", randomUUID().toString());
 	}
 

45921018407c6a6b378183f9b038c5a60f5c75af
Jonas Drewsen <jdrewsen@gmail.com>
2015-11-08 22:40:33 +0100

    Add interface for detecting codemodel and for checking supported file extension for a code intel.

--- libdeadcode/src/core/language.d
+++ libdeadcode/src/core/language.d
@@ -2,13 +2,30 @@ module core.language;
 
 import core.bufferview;
 
+import std.algorithm;
+import std.array;
+
+import std.functional; // : unaryFun, binaryFun;
+import std.range;
+
+auto findByValue(alias pred, R)(R haystack)
+//if (is(typeof(find!pred(haystack))))
+//if (isInputRange!R)// &&
+    //is (typeof(unaryFun!pred(haystack.front)) : bool))
+{
+    foreach (k, v; haystack)
+        if (pred(v))
+            return v;
+    return null;
+}
+
 class CodeIntelManager
 {
 	private
 	{
 		ICodeIntel[string] _codeIntels;
 	}
-	
+
 	void register(string name, ICodeIntel i)
 	{
 		_codeIntels[name] = i;
@@ -18,6 +35,24 @@ class CodeIntelManager
 	{
 		return _codeIntels.get(languageName, null);
 	}
+
+    ICodeIntel detect(BufferView bv)
+    {
+        return findByValue!(a => a.detect(bv))(_codeIntels);
+    }
+
+    ICodeIntel lookupByFileExtension(string ext)
+    {
+        return _codeIntels.findByValue!(a => a.IsSupportingFileExtension(ext));
+        /*
+        foreach (k, i; _codeIntels)
+        {
+            if (i.IsSupportingFileExtension(ext))
+	            return i;
+        }
+        return null;
+        */
+    }
 }
 
 static private CodeIntelManager _manager;
@@ -31,13 +66,16 @@ CodeIntelManager manager()
 
 interface ICodeIntel
 {
+    @property string languageName() const pure @safe nothrow;
+	bool detect(BufferView v);
 	ICodeModel createModel(BufferView v);
+    bool IsSupportingFileExtension(string ext);
 }
 
 interface ICodeModel
 {
 	@property ICodeIntel codeIntel();
-	@property string name() const pure nothrow @safe;	
+	@property string name() const pure nothrow @safe;
 	void updateAST();
 	string getSuggestedPath();
 }

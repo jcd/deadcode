016f85f19bcfc31a09e9c4aa474484d8d9439913
Jonas Drewsen <jdrewsen@gmail.com>
2016-01-19 16:03:46 +0100

    Add spawnProcess that writes output to log file

---
+++ libdeadcode/src/util/process.d
@@ -0,0 +1,20 @@
+module util.process;
+
+auto spawnProcess(in char[][] args,
+                  in char[] tempFileName = null,
+                  const string[string] env = null,
+                  in char[] workDir = null)
+{
+    static import std.process;
+	import std.array;
+    import std.file;
+    import std.path;
+    import std.stdio;
+    import std.typecons;
+
+    auto tempPath = buildPath(tempDir(), tempFileName);
+    auto tempLog = File(tempPath, "w+");
+    tempLog.writeln(args.join(" "));
+    auto pid = std.process.spawnProcess(args, std.stdio.stdin, tempLog, tempLog, env, std.process.Config.suppressConsole | std.process.Config.retainStdout | std.process.Config.retainStderr, workDir);
+    return tuple(pid, tempLog);
+}

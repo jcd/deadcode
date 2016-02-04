ee88a11b3199f859aca10bbb9ad4a63bfe340176
Jonas Drewsen <jdrewsen@gmail.com>
2016-02-05 00:06:11 +0100

    Added lockfree RWQueue

--- deadcodebase/util/queue.d
+++ deadcodebase/util/queue.d
@@ -104,3 +104,137 @@ unittest
             q.pop();
     }
 }
+
+
+
+/**
+* A lock-free single-reader, single-writer FIFO queue.
+* https://github.com/MartinNowak/lock-free/blob/master/src/lock_free/rwqueue.d
+*/
+shared struct RWQueue(T, size_t capacity = roundPow2!(PAGE_SIZE / T.sizeof))
+{
+    static assert(capacity > 0, "Cannot have a capacity of 0.");
+    static assert(roundPow2!capacity == capacity, "The capacity must be a power of 2");
+
+    @property size_t length() shared const
+    {
+        return atomicLoad!(MemoryOrder.acq)(_wpos) - atomicLoad!(MemoryOrder.acq)(_rpos);
+    }
+
+    @property bool empty() shared const
+    {
+        return !length;
+    }
+
+    @property bool full() const
+    {
+        return length == capacity;
+    }
+
+    void pushBusyWait(T t)
+    {
+        while (full)
+        {
+            import core.thread;
+            import core.time;
+            Thread.sleep(dur!"msecs"(10));
+        }
+        push(t);
+    }
+
+    void push(shared(T) t)
+    in { assert(!full); }
+    body
+    {
+        immutable pos = atomicLoad!(MemoryOrder.acq)(_wpos);
+        _data[pos & mask] = t;
+        atomicStore!(MemoryOrder.rel)(_wpos, pos + 1);
+    }
+
+    shared(T) pop()
+    in { assert(!empty); }
+    body
+    {
+        immutable pos = atomicLoad!(MemoryOrder.acq)(_rpos);
+        auto res = _data[pos & mask];
+        atomicStore!(MemoryOrder.rel)(_rpos, pos + 1);
+        return res;
+    }
+
+    //; Returns true if queue was not empty
+    bool clear()
+    {
+        bool startedAsEmpty = empty;
+        while (!empty)
+            pop();
+        return !startedAsEmpty;
+    }
+
+private:
+    //    import std.algorithm; // move
+
+    enum mask = capacity - 1;
+
+    size_t _wpos;
+    size_t _rpos;
+    T[capacity] _data;
+}
+
+private:
+
+enum PAGE_SIZE = 4096;
+
+template roundPow2(size_t v)
+{
+    import core.bitop : bsr;
+    enum roundPow2 = v ? cast(size_t)1 << bsr(v) : 0;
+}
+
+static assert(roundPow2!0 == 0);
+static assert(roundPow2!3 == 2);
+static assert(roundPow2!4 == 4);
+
+version (unittest)
+{
+    import core.thread, std.concurrency;
+    enum amount = 10_000;
+
+    void push(T)(ref shared(RWQueue!T) queue)
+    {
+        foreach (i; 0 .. amount)
+        {
+            while (queue.full)
+                Thread.yield();
+            queue.push(cast(shared T)i);
+        }
+    }
+
+    void pop(T)(ref shared(RWQueue!T) queue)
+    {
+        foreach (i; 0 .. amount)
+        {
+            while (queue.empty)
+                Thread.yield();
+            assert(queue.pop() == cast(shared T)i);
+        }
+    }
+}
+
+unittest
+{
+    shared(RWQueue!size_t) queue;
+    auto t0 = new Thread({push(queue);}),
+        t1 = new Thread({pop(queue);});
+    t0.start(); t1.start();
+    t0.join(); t1.join();
+}
+
+unittest
+{
+    static struct Data { size_t i; }
+    shared(RWQueue!Data) queue;
+    auto t0 = new Thread({push(queue);}),
+        t1 = new Thread({pop(queue);});
+    t0.start(); t1.start();
+    t0.join(); t1.join();
+}

099134a96d5992e9d7c6b5b8789574fedff6f849
Jonas Drewsen <jdrewsen@gmail.com>
2016-08-16 15:21:22 +0200

    Refactor GUI events to use new event system

--- libdeadcode/src/gui/event.d
+++ libdeadcode/src/gui/event.d
@@ -1,92 +1,494 @@
 module gui.event;
 
+public import dccore.event;
 import dccore.commandparameter;
 
-import derelict.sdl2.sdl;
+// import derelict.sdl2.sdl;
 import gui.keycode;
+import gui.widget : Widget;
 import math;
 
-import std.traits;
-import std.variant;
+//import std.traits;
+// import std.variant;
 
-enum EventType
-{
-	Invalid,
-	Default,
-	Command, // Command possibly with args sent
-	//	Update,
-	//	Draw,
-	MouseOver,  /// Mouse entering a widget
-	MouseMove,  /// Mouse moving over the widget
-	MouseOut,   /// Mouse exiting a widget
-	MouseDown,  /// Mouse down on a widget
-	MouseUp,    /// Mouse up on a widget
-	MouseClick, /// Mouse click on a widget
-	MouseDoubleClick, /// Mouse double click on a widget
-	MouseTripleClick, /// Mouse triple click on a widget
-	MouseScroll, // Mouse scroll wheel
-	KeyboardFocus, // When keyboard focus is obtained
-	KeyboardUnfocus, // When keyboard focus is lost
-	Text,       /// Key pressed down
-	KeyDown,    /// Key pressed down
-	KeyUp,      /// Key pressed down
-	Resize,     /// The window has been resized
-	Focus,		/// The window has been focused
-	Layout,     /// Relayout is required
-	StyleSheetChanged, // The stylesheet has been changed
-    AsyncCompletion // An async job has completed
-}
+mixin registerEvents!"GUI";
 
-string ctGenerateEventCallbacks()
+// All event types are assigned ids at runtime ie. we a not using an enum to
+// specify an event type. This makes it possible to plug in new event types from
+// extensions at runtime. This struct is simply here to register and cache all the
+// GUI specific event types.
+/*
+struct GUIEventTypes
 {
-	string str;
-	foreach (member; __traits(allMembers, EventType))
+	static void initialize(EventManager m)
 	{
-		if (member != "Invalid" && member != "Default")
+		MouseMoveEvent.staticType = m.register(EventDescription("GUI", "mouseMove"));
+		
+		string str;
+		foreach (member; __traits(allMembers, GUIEventTypes))
 		{
-			str ~= "EventUsed on" ~ member ~ "(Event event) { /*std.stdio.writeln(\"hit2 \", \"" ~ member ~ "\", name);*/ return EventUsed.no; }";
+			__traits(getMember, this, member) = m.register(EventDescription(member));
 		}
+		return str;
 	}
-	return str;
-}
 
-string ctGenerateEventCallbackSwitch()
-{
-	string str = "final switch (event.type) { ";
-	foreach (member; __traits(allMembers, EventType))
-	{
-		if (member == "Invalid" || member == "Default")
-			str ~= "case EventType." ~ member ~ ": return EventUsed.no;";
-		else
-			str ~= "case EventType." ~ member ~ ": return on" ~ member ~ "(event);";
-	}
-	str ~= "}";
-	return str;
-}
+	static EventType command;    /// Command possibly with args sent
+	static EventType mouseOver;  /// Mouse entering a widget
+	static EventType mouseMove;  /// Mouse moving over the widget
+	static EventType mouseOut;   /// Mouse exiting a widget
+	static EventType mouseDown;  /// Mouse down on a widget
+	static EventType mouseUp;    /// Mouse up on a widget
+	static EventType mouseClick; /// Mouse click on a widget
+	static EventType mouseDoubleClick; /// Mouse double click on a widget
+	static EventType mouseTripleClick; /// Mouse triple click on a widget
+	static EventType mouseScroll;      /// Mouse scroll wheel
+	static EventType keyboardFocus;    /// When keyboard focus is obtained
+	static EventType keyboardUnfocus;  /// When keyboard focus is lost
+	static EventType text;       /// Key pressed down
+	static EventType keyDown;    /// Key pressed down
+	static EventType keyUp;      /// Key pressed down
+	static EventType resize;     /// The window has been resized
+	static EventType focus;      /// The window has been focused
+	static EventType layout;     /// Relayout is required
+	static EventType styleSheetChanged; // The stylesheet has been changed
+	static EventType asyncCompletion;   // An async job has completed
+};
+*/
+//enum EventType
+//{
+//    Invalid,
+//    Default,
+//    Command, // Command possibly with args sent
+//    //	Update,
+//    //	Draw,
+//    MouseOver,  /// Mouse entering a widget
+//    MouseMove,  /// Mouse moving over the widget
+//    MouseOut,   /// Mouse exiting a widget
+//    MouseDown,  /// Mouse down on a widget
+//    MouseUp,    /// Mouse up on a widget
+//    MouseClick, /// Mouse click on a widget
+//    MouseDoubleClick, /// Mouse double click on a widget
+//    MouseTripleClick, /// Mouse triple click on a widget
+//    MouseScroll, // Mouse scroll wheel
+//    KeyboardFocus, // When keyboard focus is obtained
+//    KeyboardUnfocus, // When keyboard focus is lost
+//    Text,       /// Key pressed down
+//    KeyDown,    /// Key pressed down
+//    KeyUp,      /// Key pressed down
+//    Resize,     /// The window has been resized
+//    Focus,		/// The window has been focused
+//    Layout,     /// Relayout is required
+//    StyleSheetChanged, // The stylesheet has been changed
+//    AsyncCompletion // An async job has completed
+//}
 
-enum EventUsed
-{
-	no = 0,
-	yes = 1
-}
+//string ctGenerateEventCallbacks()
+//{
+//    string str;
+//    foreach (t; GUIEvents.EventTypes)
+//    {
+//        enum typeName = __traits(identifier, t);
+//        str ~= "EventUsed on" ~ typeName ~ "(" ~ typeName ~ ") { /*std.stdio.writeln(\"hit2 \", \"" ~ typeName ~ "\", name);*/ return EventUsed.no; }";
+//    }
+//    return str;
+//}
+
+//string ctGenerateEventCallbackSwitch()
+//{
+//    string str = "if (false) { ";
+//    foreach (t; GUIEvents.EventTypes)
+//    {
+//            enum typeName = __traits(identifier, t);
+//            enum n = identifierToEventFieldName(typeName);
+//            str ~= "} else if(event.type == GUIEvents." ~ n ~ ") {\n";
+//            str ~= "  return on" ~ typeName ~ "(cast(" ~ typeName ~ ")event);";
+//    }
+//    
+//    str ~= "} else { return EventUsed.no; }\n";
+//    return str;
+//}
+
+@nogc @safe nothrow :
 
 bool isShiftDown(KeyMod m) pure nothrow @safe
 {
-	return (m & KMOD_SHIFT) != 0;
+	return gui.keycode.isPressed(m, KeyMod.shift);
+	//return (m & KMOD_SHIFT) != 0;
 }
 
-bool isCTRLDown(KeyMod m) pure nothrow @safe
+bool isCtrlDown(KeyMod m) pure nothrow @safe
 {
-	return (m & KMOD_CTRL) != 0;
+	return gui.keycode.isPressed(m, KeyMod.ctrl);
+	//return (m & KMOD_CTRL) != 0;
 }
 
-bool isALTDown(KeyMod m) pure nothrow @safe
+bool isAltDown(KeyMod m) pure nothrow @safe @nogc
 {
-	return (m & KMOD_ALT) != 0;
+	return gui.keycode.isPressed(m, KeyMod.alt);
+	//return (m & KMOD_ALT) != 0;
 }
+
+alias WindowID = uint;
+alias MouseID = uint;
+
+enum MouseButtonFlag : byte
+{
+	left = 1,    // Button number 1
+	middle = 2,  // Button number 2
+	right = 4,   // Button number 3
+	x1 = 8,      // Button number 4
+	x2 = 16,     // Button number 5
+}
+
+bool isPressed(MouseButtonFlag state, MouseButtonFlag isThisDown) pure nothrow @safe 
+{
+	return (state & isThisDown) != 0;
+}
+
+bool isExactlyPressed(MouseButtonFlag state, MouseButtonFlag isThisDown) pure nothrow @safe
+{
+	return (state & isThisDown) != isThisDown;
+}
+
+abstract class GUIEvent : Event
+{
+	this(WindowID wid) @nogc
+	{
+		windowID = wid;
+	}
+	WindowID windowID;
+	Widget targetWidget;
+}
+
+class StyleSheetChangedEvent : GUIEvent
+{
+	this(WindowID wid)
+	{
+		super(wid);
+	}
+}
+
+// Really should be in AIO system AsyncCompletion event
+class CompletedEvent : Event
+{
+}
+
+abstract class MouseEvent : GUIEvent 
+{
+	this(WindowID wid, MouseID mid, KeyMod mods) @nogc
+	{
+		super(wid);
+		mouseID = mid;
+		modifiers = mods;
+	}
+	MouseID mouseID;
+	KeyMod modifiers;
+}
+
+interface IWidgetAware
+{
+	void setCurrentWidget(Widget w);
+}
+
+class MouseMoveEvent : MouseEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f pos, Vec2f rel, MouseButtonFlag btns) @nogc
+	{
+		super(wid, mid, mods);
+		position = pos;
+		relative = rel;
+		buttons = btns;
+	}
+
+	override @property bool allowCombine() pure @safe nothrow const
+	{
+		return true;
+	}
+
+	override bool combineIntoThis(Event ev)
+	{
+		MouseMoveEvent e = cast(typeof(this))ev;
+		if (e !is null && e.type == type && e.windowID == windowID && e.modifiers == modifiers && 
+			e.mouseID == mouseID && e.buttons == buttons)
+		{
+			position = e.position;
+			relative += e.relative;
+			return true;
+		}
+		return false;
+	}
+
+	Vec2f position;
+	Vec2f relative;
+	MouseButtonFlag buttons; // Several buttons may be set in this field
+	Widget atWidget;
+	Widget lastWidget;
+}
+
+abstract class MouseButtonEvent : MouseEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f pos, int btnChanged, MouseButtonFlag btns)
+	{
+		super(wid, mid, mods);
+		position = pos;
+		buttonChanged = btnChanged;
+		buttons = btns;
+	}
+	Vec2f position;
+	int buttonChanged;       // Button number changed (see comment for MouseButtonFlag)
+	MouseButtonFlag buttons; // Several buttons may be set in this field
+	Widget atWidget;
+}
+
+class MouseOverEvent : MouseEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Widget ovrWidget, Widget utWidget)
+	{
+		super(wid, mid, mods);
+		overWidget = ovrWidget;
+		outWidget = utWidget;
+	}
+	Widget overWidget;
+	Widget outWidget;
+}
+
+class MouseOutEvent : MouseEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Widget ovrWidget, Widget utWidget)
+	{
+		super(wid, mid, mods);
+		overWidget = ovrWidget;
+		outWidget = utWidget;
+	}
+	Widget overWidget;
+	Widget outWidget;
+}
+
+class MousePressedEvent : MouseButtonEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f pos, int btnChanged, MouseButtonFlag btn)
+	{
+		super(wid, mid, mods, pos, btnChanged, btn);
+	}
+}
+
+class MouseReleasedEvent : MouseButtonEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f pos, int btnChanged, MouseButtonFlag btn)
+	{
+		super(wid, mid, mods, pos, btnChanged, btn);
+	}
+}
+
+class MouseClickedEvent : MouseButtonEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f pos, int btnChanged, MouseButtonFlag btn, Widget ovrWidget)
+	{
+		super(wid, mid, mods, pos, btnChanged, btn);
+		overWidget = ovrWidget;
+	}
+	Widget overWidget;
+}
+
+class MouseDoubleClickedEvent : MouseButtonEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f pos, int btnChanged, MouseButtonFlag btn, Widget ovrWidget)
+	{
+		super(wid, mid, mods, pos, btnChanged, btn);
+		overWidget = ovrWidget;
+	}
+	Widget overWidget;
+}
+
+class MouseTripleClickedEvent : MouseButtonEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f pos, int btnChanged, MouseButtonFlag btn, Widget ovrWidget)
+	{
+		super(wid, mid, mods, pos, btnChanged, btn);
+		overWidget = ovrWidget;
+	}
+	Widget overWidget;
+}
+
+
+class MouseWheelEvent : MouseEvent
+{
+	this(WindowID wid, MouseID mid, KeyMod mods, Vec2f scrl, bool scrollIsFlipped)
+	{
+		super(wid, mid, mods);
+		scroll = scrl;
+		isScrollFlipped = scrollIsFlipped;
+	}
+	Vec2f scroll;
+	bool isScrollFlipped;
+
+	override @property bool allowCombine() pure @safe nothrow const
+	{
+		return true;
+	}
+
+	override bool combineIntoThis(Event ev)
+	{
+		MouseWheelEvent e = cast(typeof(this))ev;
+		if (e !is null && e.type == type && e.windowID == windowID && e.modifiers == modifiers && 
+			e.mouseID == mouseID && e.isScrollFlipped == isScrollFlipped)
+		{
+			scroll += e.scroll;
+			return true;
+		}
+		return false;
+	}
+}
+
+class InputFocusEvent : GUIEvent
+{
+	this(WindowID wid)
+	{
+		super(wid);
+	}
+}
+
+class InputUnfocusEvent : GUIEvent
+{
+	this(WindowID wid)
+	{
+		super(wid);
+	}
+}
+
+// GUIEvent really?
+class CommandEvent : GUIEvent
+{
+	this(string commandName, CommandParameter[] args, WindowID wid)
+	{
+		super(wid);
+		this.commandName = commandName;
+		arguments = args;
+	}
+	
+	string commandName;
+	CommandParameter[] arguments;
+}
+
+abstract class KeyboardEvent : GUIEvent
+{
+	this(WindowID wid, KeyMod mods)
+	{
+		super(wid);
+		modifiers = mods;
+	}
+
+	KeyMod modifiers;
+}
+
+abstract class KeyCharEvent : KeyboardEvent
+{
+	this(WindowID wid, KeyMod mods, KeyCode keycode, dchar c)
+	{
+		super(wid, mods);
+		code = keycode;
+		unicodeChar = c;
+	}
+	KeyCode code;
+	dchar unicodeChar;
+}
+
+class KeyPressedEvent : KeyCharEvent
+{
+	this(WindowID wid, KeyMod mods, KeyCode keycode, dchar c)
+	{
+		super(wid, mods, keycode, c);
+	}
+}
+
+class KeyReleasedEvent : KeyCharEvent
+{
+	this(WindowID wid, KeyMod mods, KeyCode keycode, dchar c)
+	{
+		super(wid, mods, keycode, c);
+	}
+}
+
+class TextEvent : KeyboardEvent
+{
+	this(WindowID wid, KeyMod mods, dchar unicodeChr)
+	{
+		super(wid, mods);
+		unicodeChar = unicodeChr;
+	}
+	dchar unicodeChar;
+}
+
+abstract class WindowEvent : GUIEvent
+{
+	this(uint wid)
+	{
+		super(wid);
+	}
+}
+
+class WindowResizedEvent : WindowEvent
+{
+	this(uint _windowID, Vec2f sz)
+	{
+		super(_windowID);
+		size = sz;
+	}
+	Vec2f size;
+}
+
+class WindowFocussedEvent : WindowEvent
+{
+	this(uint _windowID)
+	{
+		super(_windowID);
+	}
+}
+
+class WindowUnfocussedEvent : WindowEvent
+{
+	this(uint _windowID)
+	{
+		super(_windowID);
+	}
+}
+
+// TODO: move out of here
+class DropFile : Event
+{
+	this (string f, KeyMod mods)
+	{
+		file = f;
+		modifiers = mods;
+	}
+	string file;
+	KeyMod modifiers;
+}
+
+//    MouseOver,  /// Mouse entering a widget
+//    MouseMove,  /// Mouse moving over the widget
+//    MouseOut,   /// Mouse exiting a widget
+//    MouseDown,  /// Mouse down on a widget
+//    MouseUp,    /// Mouse up on a widget
+//    MouseClick, /// Mouse click on a widget
+//    MouseDoubleClick, /// Mouse double click on a widget
+//    MouseTripleClick, /// Mouse triple click on a widget
+//    MouseScroll, // Mouse scroll wheel
+//    KeyboardFocus, // When keyboard focus is obtained
+//    KeyboardUnfocus, // When keyboard focus is lost
+//    Text,       /// Key pressed down
+//    KeyDown,    /// Key pressed down
+//    KeyUp,      /// Key pressed down
+//    Resize,     /// The window has been resized
+//    Focus,		/// The window has been focused
+//    Layout,     /// Relayout is required
+//    StyleSheetChanged, // The stylesheet has been changed
+//    AsyncCompletion // An async job has completed
+
 
 // TODO: add pointer to active widget in here ie. refactor this struct to new file
-struct Event
+/*
+struct EventX
 {
 	enum MouseButton : byte
 	{
@@ -120,7 +522,7 @@ struct Event
 			case EventType.MouseOut:   /// Mouse exiting a widget
 				goto case;
 			case EventType.MouseDown:  /// Mouse down on a widget
-				goto case;
+				goto case;m
 			case EventType.MouseUp:    /// Mouse up on a widget
 				goto case;
 			case EventType.MouseClick: /// Mouse click on a widget
@@ -213,7 +615,7 @@ struct Event
 	}
 }
 
-class EventQueue
+class EventQueueOld
 {
 	Event event;
 	EventQueue next;
@@ -241,3 +643,4 @@ class EventQueue
 		return next is null;
 	}
 }
+*/
